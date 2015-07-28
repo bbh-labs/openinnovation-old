a7b84c9838c887517fdecd2af7ad9d59936b2bdd
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-07-28 11:22:35 +0800

    Update
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- main.go
+++ main.go
@@ -5,28 +5,9 @@ import (
 
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
-	"github.com/throttled/throttled"
 
 	_ "bbhoi.com/store"
 )
-
-var (
-	LowThrottle = throttled.Interval(throttled.PerSec(10), 100, &throttled.VaryBy{Path: true}, 50)
-	MediumThrottle = throttled.Interval(throttled.PerSec(5), 50, &throttled.VaryBy{Path: true}, 25)
-	HighThrottle = throttled.Interval(throttled.PerSec(1), 10, &throttled.VaryBy{Path: true}, 5)
-)
-
-func lt(f http.HandlerFunc) http.Handler {
-	return LowThrottle.Throttle(http.Handler(f))
-}
-
-func mt(f http.HandlerFunc) http.Handler {
-	return MediumThrottle.Throttle(http.Handler(f))
-}
-
-func ht(f http.HandlerFunc) http.Handler {
-	return HighThrottle.Throttle(http.Handler(f))
-}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	r.Header.Set("Content-Type", "text/html")
@@ -35,23 +16,23 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	router := mux.NewRouter()
-	router.Handle("/", lt(home))
-	router.Handle("/login", mt(login))
-	router.Handle("/register", mt(register))
-	router.Handle("/verify", ht(verify))
+	router.HandleFunc("/", home)
+	router.HandleFunc("/login", login)
+	router.HandleFunc("/register", register)
+	router.HandleFunc("/verify", verify)
 
 	apiRouter := mux.NewRouter()
-	apiRouter.Handle("/api/logout", mt(logout))
-	apiRouter.Handle("/api/user", mt(user))
-	apiRouter.Handle("/api/project", mt(project))
+	apiRouter.HandleFunc("/api/logout", logout)
+	apiRouter.HandleFunc("/api/user", user)
+	apiRouter.HandleFunc("/api/project", project)
 	router.PathPrefix("/api").Handler(negroni.New(
 		negroni.HandlerFunc(apiMiddleware),
 		negroni.Wrap(apiRouter),
 	))
 
 	adminRouter := mux.NewRouter()
-	adminRouter.Handle("/api/admin/user", ht(adminUser))
-	adminRouter.Handle("/api/admin/project", ht(adminProject))
+	adminRouter.HandleFunc("/api/admin/user", adminUser)
+	adminRouter.HandleFunc("/api/admin/project", adminProject)
 	router.PathPrefix("/api/admin").Handler(negroni.New(
 		negroni.HandlerFunc(adminMiddleware),
 		negroni.Wrap(adminRouter),
