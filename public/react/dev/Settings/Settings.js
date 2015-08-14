fba36e63ff0aed9c7521164a950fcea6dcf855f2
Jacky Boen <jacky.boen@bartleboglehegarty.com>
2015-08-14 17:15:11 +0800

    Update
    Signed-off-by: Jacky Boen <jacky.boen@bartleboglehegarty.com>

--- public/react/dev/Settings/Settings.js
+++ public/react/dev/Settings/Settings.js
@@ -1,8 +1,35 @@
 var Settings = React.createClass({
 	render: function() {
 		return (
-			<div>
-			</div>
+			<main>
+				<div className="row">
+					<div className="container">
+						<Settings.Form />
+					</div>
+				</div>
+			</main>
 		)
+	},
+});
+
+Settings.Form = React.createClass({
+	render: function() {
+		var mailFilter = OI.getMailPreference();
+		var active = mailFilter && mailFilter.length > 0 ? "active" : "";
+		return (
+			<form className="col s12" onSubmit={this.handleSubmit}>
+				<div className="input-field col s12">
+					<input id="mail-filter" type="text" name="mail-filter" defaultValue={mailFilter} />
+					<label htmlFor="mail-filter" className={active}>Mail Filter</label>
+				</div>
+				<button className="btn waves-effect waves-light">Save Settings</button>
+			</form>
+		)
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+
+		var mailFilter = e.target.elements["mail-filter"].value;
+		OI.setMailPreference(mailFilter);
 	},
 });
--- public/react/prod/Settings/Settings.js
+++ public/react/prod/Settings/Settings.js
@@ -1,8 +1,35 @@
 var Settings = React.createClass({displayName: "Settings",
 	render: function() {
 		return (
-			React.createElement("div", null
+			React.createElement("main", null, 
+				React.createElement("div", {className: "row"}, 
+					React.createElement("div", {className: "container"}, 
+						React.createElement(Settings.Form, null)
+					)
+				)
 			)
 		)
+	},
+});
+
+Settings.Form = React.createClass({displayName: "Form",
+	render: function() {
+		var mailFilter = OI.getMailPreference();
+		var active = mailFilter && mailFilter.length > 0 ? "active" : "";
+		return (
+			React.createElement("form", {className: "col s12", onSubmit: this.handleSubmit}, 
+				React.createElement("div", {className: "input-field col s12"}, 
+					React.createElement("input", {id: "mail-filter", type: "text", name: "mail-filter", defaultValue: mailFilter}), 
+					React.createElement("label", {htmlFor: "mail-filter", className: active}, "Mail Filter")
+				), 
+				React.createElement("button", {className: "btn waves-effect waves-light"}, "Save Settings")
+			)
+		)
+	},
+	handleSubmit: function(e) {
+		e.preventDefault();
+
+		var mailFilter = e.target.elements["mail-filter"].value;
+		OI.setMailPreference(mailFilter);
 	},
 });
